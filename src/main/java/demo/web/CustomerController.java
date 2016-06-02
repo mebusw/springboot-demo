1009815f1d6d3f9f080e456aa566d564874e0aa5
mebusw <mebusw@163.com>
2016-06-02 23:16:29 +0800

    add more nested RequestMapping to read remote URL and parse json string to object

--- src/main/java/demo/web/CustomerController.java
+++ src/main/java/demo/web/CustomerController.java
@@ -1,23 +1,83 @@
 package demo.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 @RestController
 @RequestMapping(value = "/customers", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CustomerController {
-    @RequestMapping("/")
+    @RequestMapping(value = "/")
     @ResponseBody
     List customers() {
-        return Arrays.asList(1,2,3);
+        return Arrays.asList(1, 2, 3);
     }
 
+
     @RequestMapping(value = "/{customer}", method = RequestMethod.GET)
-    String getCustomer(@PathVariable Long customer) {
-        return "CustomerController = " + customer;
+    @ResponseBody
+    String getCustomer(@PathVariable("customer") Long customer) {
+        return "CustomerController.getCustomer = " + customer;
+    }
+
+
+    @RequestMapping(value = "/objmap", method = RequestMethod.GET)
+    @ResponseBody
+    String objmap() {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = "{\"name\":\"mkyong\", \"age\":123}";
+        try {
+            Staff obj = mapper.readValue(jsonInString, Staff.class);
+            System.out.println(obj.getName());
+            System.out.println(obj.getAge());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "objmap";
+    }
+
+    @RequestMapping(value = "/commits", method = RequestMethod.GET)
+    @ResponseBody
+    String getCommits() {
+        String url = "https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha=";
+        try {
+            InputStream resp = new URL(url).openStream();
+            Scanner scanner = new Scanner(resp);
+            String respBody = scanner.useDelimiter("\\A").next();
+            return respBody;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Error reading from github api";
     }
 
 }
+
+class Staff {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
