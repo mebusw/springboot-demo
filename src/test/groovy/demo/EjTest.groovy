07e16c5c8f6f341f186bff27021a4d991dd14c88
mebusw <mebusw@163.com>
2016-06-29 17:21:42 +0800

    Sample of writting test case in Groovy lang with JUnit annotations.

---
+++ src/test/groovy/demo/EjTest.groovy
@@ -0,0 +1,16 @@
+package demo
+import org.junit.Before
+import org.junit.Test
+
+import static junit.framework.TestCase.assertEquals
+
+class EjTest  {
+    @Before
+    void setUp() {
+    }
+
+    @Test
+    void test1() {
+        assertEquals 1, 1
+    }
+}
